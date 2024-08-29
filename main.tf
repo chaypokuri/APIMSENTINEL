db1c8252ee28dc643915b41383e52c45ffcad75a
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-08-29 12:36:51 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
+tenant_id = "3180c264-31bc-4113-8f50-b7393a40457b"
+client_id = "1a046c02-8c39-4f1d-b30b-93f41a9c6b15"
+client_secret = "kUz8Q~qwom0J-MM5ZNqexXyUOguygMj5QELdhdl5"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_api_management" "example" {
+  name                = "example-apim"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "My Company"
+  publisher_email     = "company@terraform.io"
+
+  sku_name = "Developer_1"
+}
