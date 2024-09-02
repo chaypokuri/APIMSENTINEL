8ef67a4207fe867d1c7e0b981f5e53ac7dac1f3f
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-02 13:30:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,12 +19,30 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
+resource "azurerm_virtual_network" "example" {
+  name                = "example-virtual-network"
+  address_space       = ["10.0.0.0/24"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+ 
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/27"]
+}
+
 resource "azurerm_api_management" "example" {
   name                = "example-apim"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   publisher_name      = "My Company"
   publisher_email     = "company@terraform.io"
-
   sku_name = "Developer_1"
+  virtual_network_configuration {
+  subnet_id = azurerm_subnet.example.id
+  }
 }
+
+
